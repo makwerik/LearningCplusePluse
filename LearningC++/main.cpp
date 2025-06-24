1e6eac932f4409dd6f0ff3a9dd7dccb5d99f46a6
makwerik <s.verevkin1999@gmail.com>
2025-06-24 22:18:40 +0300

    База

--- LearningC++/main.cpp
+++ LearningC++/main.cpp
@@ -1,7 +1,8 @@
 ﻿#include <iostream>
 #include "First/first.h"
 #include <locale>
-
+#include "Twoo/twoo.h"
+#include <string>
 
 int main() {
 	setlocale(LC_ALL, "");
@@ -12,6 +13,20 @@ int main() {
 		/* Словарь без сортировки по ключам
 			setPeopleUnorderMap();
 		*/
-	setPeopleUnorderMap();
+
+	/* Twoo:/ 
+		Множество с уникальными значениями и сортировкой
+		const int size = 5;
+
+		std::string books[size] = { "Book1", "Book2", "Book3", "Book3", "Book5" };
+
+		setExample(books, size);
+
+		Такой же метод, но без сортировки
+			const int size = 5;
+			unorderedSetExample(size);
+	*/
+
+
 	return 0;
 }
