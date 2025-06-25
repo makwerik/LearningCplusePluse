bdbb5b758bda270e6ab6943ee7b14b434b35268c
makwerik <s.verevkin1999@gmail.com>
2025-06-25 23:08:23 +0300

    Сложные коллекции, к примеру словарь со списком любмых блюд пользователей

---
+++ LearningC++/Three/three.cpp
@@ -0,0 +1,43 @@
+﻿#include "three.h"
+#include <locale>
+#include <map>
+#include <vector>
+#include <string>
+#include <iostream>
+
+void setDishes() {
+	// Создаю хранилище хранения любимых блюд у разных людей
+	std::map<std::string, std::vector<std::string>> dishes;
+	std::string name;
+	std::string dishe;
+	const int SIZE_NAMES = 2;
+	int size_dishes;
+	
+	for (int i = 0; i < SIZE_NAMES; i++) {
+
+		std::cout << "Введите имя: ";
+		std::cin >> name;
+		std::cout << "Введите кол-во блюд для " << name <<": ";
+		std::cin >> size_dishes;
+
+		std::cin.ignore();
+		for (int j = 0; j < size_dishes; j++) {
+			std::cout << "Введите " << j + 1 << "е блюдо: ";
+			std::getline(std::cin, dishe);
+
+			dishes[name].push_back(dishe);
+
+		}
+		std::cout << "\n" << std::endl;
+
+	}
+
+	// Вывод информации
+	for (const auto& pair : dishes) {
+		std::cout << pair.first << " и его любимые блюда:" << std::endl;
+		for (const auto& sec : pair.second) {
+			std::cout << "- " << sec << std::endl;
+		}
+	}
+
+}
